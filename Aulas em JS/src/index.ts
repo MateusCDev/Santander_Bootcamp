3d313baed3bd29b70cc51f74747005453af354d1
MateusCDev <mateus.cesararaujo@hotmail.com>
2022-07-01 16:22:33 -0300

    Praticando TypeScript e organizando pastaso

---
+++ Aulas em JS/src/index.ts
@@ -0,0 +1,38 @@
+/*const input = document.getElementById('input') as HTMLInputElement;
+input.addEventListener('input', (event) => {
+    
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+})*/
+
+/*function dados <T>(arr: T[], valor:T){
+    return arr.map(() => valor);
+
+}
+console.log(dados([1,2,3], 10));*/
+
+/*interface IUsuario{
+    id: string;
+    email: string;
+    cargo?: 'funcionario' |'gerente'|'coordenador'| 'supervisor';
+}
+function redirecione (usuario: IUsuario){
+    if(usuario.cargo){
+         //redirecionar(usuario.cargo);
+    }
+    //redirecionar para a área do usuário
+}*/
+
+/*interface Pessoa{
+    nome: string;
+    idade: number;
+    nacionalidade: string;
+}
+
+interface Brasileiro extends Omit<Pessoa, 'nacionalidade'>{
+
+}
+
+const brasileiro: Brasileiro ={
+    
+}*/
